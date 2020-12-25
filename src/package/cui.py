c871dbfec78af640bf0791fc3f2000b586c825c8
purmirl <gmlrnjs1994@gmail.com>
2020-12-25 20:03:09 +0900

    making

---
+++ src/package/cui.py
@@ -0,0 +1,36 @@
+"""
+ ProbeArrow
+ Copyright 2020 PeTrA. All rights reserved.
+ . Python Project Structure Repository;
+
+ Probe Arrow Project by PeTrA. 2020
+ ProbeArrow 1.0
+ Language : Python3.8.2
+ Library : Scapy2.4.3
+
+ Advanced Trace Route
+ ------
+ @ cui.py
+    * character user interface python code file
+"""
+
+class Cui():
+
+    def __init__(self):
+        return
+
+    def cui_engine(self):
+
+        while True:
+
+
+    def print_rights(self):
+        print("Copyright 2020 PeTrA. All rights reserved.")
+        print("ProbeArrow 1.0\n")
+        return
+
+    def get_command(self, _layer_name):
+        result = ""
+        result = input(_layer_name + "@probearrow:~# ")
+
+        return result
