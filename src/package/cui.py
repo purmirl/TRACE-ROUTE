8e7cfcb1a70e22f6c813547112b66b9997104c1b
purmirl <gmlrnjs1994@gmail.com>
2020-12-27 18:09:01 +0900

    cui.py is modified.

--- src/package/cui.py
+++ src/package/cui.py
@@ -13,6 +13,8 @@
  @ cui.py
     * character user interface python code file
 """
+from src.package.function import is_protocol_address
+
 
 class Cui():
 
@@ -28,8 +30,40 @@ class Cui():
                 self.print_main_option()
                 continue
             elif HIVE_MAIN_COMMAND == "traceroute":
+                while True:
+                    HIVE_TRACEROUTE_COMMAND = self.get_command("traceroute")
+                    if HIVE_TRACEROUTE_COMMAND == "?":
+                        self.print_traceroute_option()
+                        continue
+                    elif HIVE_TRACEROUTE_COMMAND == "":
+                        continue
+                    elif HIVE_TRACEROUTE_COMMAND == "quit":
+                        break
+                    else: # something is put.
+                        is_ip_address = is_protocol_address(HIVE_TRACEROUTE_COMMAND)
+                        if is_ip_address == 0:
+                            self.print_ip_error()
+                            continue
+                        ###########################################################################
+                        # here, traceroute function is needed.
+                        ###########################################################################
+                        continue
+                    continue
                 continue
             elif HIVE_MAIN_COMMAND == "show":
+                while True:
+                    HIVE_SHOW_COMMAND = self.get_command("show")
+                    if HIVE_SHOW_COMMAND == "?":
+                        self.print_show_option()
+                        continue
+                    elif HIVE_SHOW_COMMAND == "version":
+                        # print software version
+                        continue
+                    elif HIVE_SHOW_COMMAND == "quit":
+                        break
+                    else:
+                        continue
+                    continue
                 continue
             elif HIVE_MAIN_COMMAND == "exit":
                 break
@@ -61,6 +95,24 @@ class Cui():
                       " 02. quit : quit show option \n" \
                       ""
         print(SHOW_OPTION)
+        return
+
+    """ traceroute option
+            [traget ip address]
+    """
+    def print_traceroute_option(self):
+        TRACEROUTE_OPTION = "\n" \
+                            " 01. [target ip address] : write target ip address for search \n" \
+                            " 02. quit : quit traceroute option \n" \
+                            ""
+        print(TRACEROUTE_OPTION)
+        return
+
+    def print_ip_error(self):
+        IP_ERROR_STRING = "\n" \
+                          " This is not Internet Protocol address. \n"\
+                          ""
+        print(IP_ERROR_STRING)
         return
 
     def print_rights(self):
